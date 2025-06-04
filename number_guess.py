178b01e1389e3d6c668c03fb663b82c65e436522
Ample1827 <Adalromero99@gmail.com>
2025-06-04 15:06:21 -0600

    fix: remove redundant input validation for number guessing range

--- number_guess.py
+++ number_guess.py
@@ -14,9 +14,6 @@ class Game:
         while True:
             try:
                 guess = int(input("Guess a number between 1 and 100: "))
-                if guess < 1 or guess > 100:
-                    print("Invalid input. Please enter a number between 1 and 100.")
-                    continue
                 if guess == n:
                     print("Congratulations! You've guessed the number!")
                     break
