6e0136a7007db7a776e7a88f8313363ffde69e18
Ample1827 <Adalromero99@gmail.com>
2025-06-05 13:11:04 -0600

    refactor: move number generation to play method in Game class

--- number_guess.py
+++ number_guess.py
@@ -1,10 +1,8 @@
 import random
 
 class Game:
-    def __init__(self):
-        self.number_to_guess = random.randint(1, 100)
-       
     def play(self):
+        self.number_to_guess = random.randint(1, 100)
         while True:
             try:
                 guess = int(input("Guess a number between 1 and 100: "))
